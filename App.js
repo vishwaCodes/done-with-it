b59b38b8b99a33547ce663d711d2dbf1ee49ddb8
candytuft <vishnyc3@gmail.com>
2021-03-18 14:57:34 -0400

    Adds Flex Direction

--- App.js
+++ App.js
@@ -11,24 +11,28 @@ export default function App() {
       style={{
         backgroundColor: "#fff",
         flex: 1,
+        flexDirection: "row",
       }}
     >
       <View
         style={{
           backgroundColor: "dodgerblue",
-          flex: 1,
+          width: 100,
+          height: 100,
         }}
       />
       <View
         style={{
           backgroundColor: "gold",
-          flex: 1,
+          width: 100,
+          height: 100,
         }}
       />
       <View
         style={{
           backgroundColor: "tomato",
-          flex: 1,
+          width: 100,
+          height: 100,
         }}
       />
     </View>
