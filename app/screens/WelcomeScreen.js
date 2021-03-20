d42f51b14241909a2f0759b8fa2a0d22de7a1a3c
candytuft <vishnyc3@gmail.com>
2021-03-19 21:00:58 -0400

    Adds styles to Welcome Screen

--- app/screens/WelcomeScreen.js
+++ app/screens/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ImageBackground, StyleSheet, View } from "react-native";
+import { ImageBackground, StyleSheet, View, Image, Text } from "react-native";
 
 function WelcomeScreen(props) {
   return (
@@ -7,6 +7,13 @@ function WelcomeScreen(props) {
       style={styles.background}
       source={require("../assets/background.jpg")}
     >
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+        <Text>
+          Best place to sell the stuff you don't need anymore! We have made the
+          process lightening fast, easy-peasy, and secure, just for you!
+        </Text>
+      </View>
       <View style={styles.loginButton}></View>
       <View style={styles.registerButton}></View>
     </ImageBackground>
@@ -17,16 +24,26 @@ const styles = StyleSheet.create({
   background: {
     flex: 1,
     justifyContent: "flex-end",
+    alignItems: "center",
   },
   loginButton: {
     width: "100%",
     height: 70,
     backgroundColor: "#fc5c65",
   },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
   registerButton: {
     width: "100%",
     height: 70,
     backgroundColor: "#4ecdc4",
+  },
+  logo: {
+    width: 100,
+    height: 100,
   },
 });
 
